fe90a6545d4df56a0e0af737783d32a76adde3c7
Cameron Baughn <cam.baughn@gmail.com>
2021-10-05 23:17:48 -0700

    Stub out home component and firebase connection

---
+++ src/Home/Home.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import styles from './Home.module.scss';
+
+export default function Home({}) {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
---
+++ util/firebase/firebaseInit.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import firebaseConfig from './firebaseConfig';
+// require('firebase/firestore')
+// const firebaseConfig = require('./firebaseConfig');
+
+
+let app;
+try {
+  app = initializeApp(firebaseConfig);
+} catch(error) {
+  if (!/already exists/.test(error.message)) {
+    console.error('Firebase initialization error', error.stack)
+  }
+}
+
+// Initialize Cloud Firestore through Firebase
+let db = getFirestore(app);
+
+module.exports = db;
