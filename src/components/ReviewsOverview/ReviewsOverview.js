bea66e9b5cb451601aab56eebe69d832d5f8b8a0
Cameron Baughn <cam.baughn@gmail.com>
2021-10-06 06:56:42 -0700

    Add "add review" button to Review Overview component

--- src/components/ReviewsOverview/ReviewsOverview.js
+++ src/components/ReviewsOverview/ReviewsOverview.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './ReviewsOverview.module.scss';
 
 
-export default function ReviewsOverview({ product = {}, reviews = [] }) {
+export default function ReviewsOverview({ product = {}, reviews = [], openReviewModal }) {
+
   return (
     <div className={styles.container}>
       <h1 className={styles.productName}>{product.name}</h1>
@@ -11,12 +12,22 @@ export default function ReviewsOverview({ product = {}, reviews = [] }) {
           <span className={styles.averageRating}>3.8</span>
           <div className={styles.stars}></div>
         </div>
+
+        <div className={styles.addReviewButton} onClick={openReviewModal}>Add review</div>
       </div>
 
       <div className={styles.divider}></div>
 
       <div className={styles.reviews}>
         <h3 className={styles.reviewHeader}>Reviews</h3>
+        { reviews.map((review, index) => {
+          return (
+            <div className={styles.reviewWrapper}>
+              <div className={styles.individualReviewStars}></div>
+              <span className={styles.reviewText}><span className={styles.reviewRating}>{review.rating}</span>{review.text}</span>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
