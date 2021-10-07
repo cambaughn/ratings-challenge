57291663947c1e9cc8dc15c1a3e54f46e953bafe
Cameron Baughn <cam.baughn@gmail.com>
2021-10-07 12:40:15 -0700

    Allow for rating system to update on hover

--- src/components/StarRating/StarRating.js
+++ src/components/StarRating/StarRating.js
@@ -1,27 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import styles from './StarRating.module.scss';
+import classNames from 'classnames';
 import { determineFullStars, hasHalfStar, determineEmptyStars, roundHalf } from '../../util/helpers/stars';
 import { Star, StarBorder, StarHalf } from '@material-ui/icons';
-
+import { v4 as uuidv4 } from 'uuid';
 
 export default function StarRating({ rating, setRating, clickable }) {
   const [fullStars, setFullStars] = useState(0);
   const [halfStar, setHalfStar] = useState(false);
   const [emptyStars, setEmptyStars] = useState(0);
   const [stars, setStars] = useState([]);
-
-  const determineRating = () => {
-    if (rating) {
-      let roundedRating = roundHalf(rating);
-      setEmptyStars(determineEmptyStars(roundedRating));
-      setFullStars(determineFullStars(roundedRating));
-      setHalfStar(hasHalfStar(roundedRating));
-    }
-  }  
+  const [virtualRating, setVirtualRating] = useState(null);
   
+  const resetVirtualRating = () => { // reset the virtual rating to null after the mouse leaves the clickable area
+    setVirtualRating(null);
+  }
+
+  const determineMousePosition = (event, index) => {
+    let width = event.currentTarget.offsetWidth;
+    let bounds = event.target.getBoundingClientRect();
+    let x = event.clientX - bounds.left;
+    // let y = event.clientY - bounds.top;
+    let halfStar = x < (width / 2);
+    let newVirtualRating = halfStar ? index + 0.5 : index + 1;
+    setVirtualRating(newVirtualRating);
+    console.log(halfStar);
+  }
+
   const determineStars = () => {
     if (rating) {
-      let currentRating = roundHalf(rating);
+      let currentRating = virtualRating ? roundHalf(virtualRating) : roundHalf(rating);
       let starsToShow = [];
 
       for (let i = 0; i < 5; i++) {
@@ -41,17 +49,30 @@ export default function StarRating({ rating, setRating, clickable }) {
     }
   }
 
-  useEffect(determineStars, [rating]);
+  useEffect(determineStars, [rating, virtualRating]);
+
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onMouseOut={resetVirtualRating}>
       { stars.map((star, index) => {
         if (star === 1) {
-          return <Star className={styles.star} key={index} />
+          return (
+            <div className={classNames({ [styles.clickableStar]: clickable })} onMouseMove={(event) => determineMousePosition(event, index)} key={uuidv4()}>
+              <Star className={styles.star} key={uuidv4()} />
+            </div>
+          )
         } else if (star === 0.5) {
-          return <StarHalf className={styles.star} key={index} />
+          return (
+            <div className={classNames({ [styles.clickableStar]: clickable })} onMouseMove={(event) => determineMousePosition(event, index)} key={uuidv4()}>
+              <StarHalf className={styles.star}  />
+            </div>
+          )
         } else {
-          return <StarBorder className={styles.star} key={index} />
+          return (
+            <div className={classNames({ [styles.clickableStar]: clickable })} onMouseMove={(event) => determineMousePosition(event, index)} key={uuidv4()}>
+              <StarBorder className={styles.star} />
+            </div>
+          )
         }
       })}
     </div>
